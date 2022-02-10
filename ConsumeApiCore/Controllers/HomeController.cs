8698d6433af1ed7db6c51082934b3e772194c7b9
Murali Krishnamurthy <muralimk@outlook.com>
2022-02-10 17:33:08 +0530

    without delete operation

---
+++ ConsumeApiCore/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using ConsumeApiCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ConsumeApiCore.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ConsumeApiCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Emp> empList = new List<Emp>();
+            using (var httpClient = new HttpClient())
+            {
+                using ( var response = await httpClient.GetAsync("http://localhost:85/api/employee"))
+                {
+                    string apiresponse = await response.Content.ReadAsStringAsync();
+                    empList = JsonConvert.DeserializeObject<List<Emp>>(apiresponse);
+                }
+            }
+            return View(empList);
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Emp employee)
+        {
+            Emp emp = new Emp();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(employee),Encoding.UTF8,"application/json");
+
+                using (var response = await httpClient.PostAsync("http://localhost:85/api/employee",content))
+                {
+                    string apiresponse = await response.Content.ReadAsStringAsync();
+                    emp  =JsonConvert.DeserializeObject<Emp>(apiresponse);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> deleteemp(int empid)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("http://localhost:85/api/employee/" + empid))
+                {
+                    string apiresponse = await response.Content.ReadAsStringAsync();
+                }
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ConsumeApiCore/Models/Emp.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsumeApiCore.Models
+{
+    public class Emp
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public double EmpSalary { get; set; }
+
+    }
+}
